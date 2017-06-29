2cc0d1dd7d4ceee7909af3243d103628029f5ae1
Rodrigo F. Fernandes <rodrigoff@live.com>
2017-06-28 21:08:48 -0300

    Adds RatingControl

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController, UITextFieldDelegate,
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -56,10 +57,6 @@ class ViewController: UIViewController, UITextFieldDelegate,
     }
     
     // MARK: Actions
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        mealNameLabel.text = "Default Text"
-    }
-
     @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
         // Hide the keyboard.
         nameTextField.resignFirstResponder()
