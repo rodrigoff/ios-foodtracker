1309f37a17353ee0cbeb52e87102beed427801cc
Rodrigo F. Fernandes <rodrigoff@live.com>
2017-07-01 10:56:18 -0300

    Adds Meal data model

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,38 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Rodrigo F. Fernandes on 01/07/17.
+//  Copyright © 2017 Rodrigo F. Fernandes. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    
+    //MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    //MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // The rating must be between 0 and 5 inclusively
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
+        // Initialize store properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+    
+}
