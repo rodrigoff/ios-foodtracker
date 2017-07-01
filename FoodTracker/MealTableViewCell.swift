309593d0f1bf7c55c86c7ec16c4a6489f478c61d
Rodrigo F. Fernandes <rodrigoff@live.com>
2017-07-01 15:59:11 -0300

    Adds navigation to the New Meal view

--- FoodTracker/MealTableViewCell.swift
+++ FoodTracker/MealTableViewCell.swift
@@ -11,7 +11,6 @@ import UIKit
 class MealTableViewCell: UITableViewCell {
     
     //MARK: Properties
-    
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 class MealTableViewController: UITableViewController {
     
     //MARK: Properties
-    
     var meals = [Meal]()
 
     override func viewDidLoad() {
@@ -98,8 +97,19 @@ class MealTableViewController: UITableViewController {
     }
     */
     
-    //MARK: Private Methods
+    //MARK: Actions
+    @IBAction func unwindToMealList(sender: UIStoryboardSegue) {
+        if let sourceViewController = sender.source as? MealViewController, let meal = sourceViewController.meal {
+            // Add a new meal.
+            let newIndexPath = IndexPath(row: meals.count, section: 0)
+            
+            meals.append(meal)
+            
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        }
+    }
     
+    //MARK: Private Methods
     private func loadSampleMeals() {
         let photo1 = UIImage(named: "meal1")
         let photo2 = UIImage(named: "meal2")
